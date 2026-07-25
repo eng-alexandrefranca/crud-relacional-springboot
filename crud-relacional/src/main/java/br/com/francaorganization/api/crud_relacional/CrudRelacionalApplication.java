4f5b5f5ff714b1464980ce6d958dfadfc020290a
z303007 <alexandredasilvafranca@hotmail.com>
2026-07-25 02:44:00 -0300

    Estrutura inicial do projeto e pastas MVC

---
+++ crud-relacional/src/main/java/br/com/francaorganization/api/crud_relacional/CrudRelacionalApplication.java
@@ -0,0 +1,13 @@
+package br.com.francaorganization.api.crud_relacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRelacionalApplication.class, args);
+	}
+
+}
